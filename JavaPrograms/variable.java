b1deeb620df1c386156c39c3e4614e60ea260e11
shrutimadduru <mvss97@gmail.com>
2022-06-11 18:33:09 -0400

    variables

---
+++ JavaPrograms/variable.java
@@ -0,0 +1,17 @@
+class variable
+{
+	public static void main (String[] args){
+
+		int a=401;
+		System.out.println("a = "+a);
+		byte b;
+		b=105;
+		System.out.println("b = "+b);
+		long l;
+		l = 66985733;
+		System.out.println("l = "+l);
+		boolean S = true;
+		System.out.println("S = "+S);
+		
+ 	}	
+}
