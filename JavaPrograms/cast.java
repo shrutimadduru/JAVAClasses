1cef390cf01248bb8df9c613abdf26e4c572ff36
shrutimadduru <mvss97@gmail.com>
2022-07-10 18:19:51 -0400

    typecasting

---
+++ JavaPrograms/cast.java
@@ -0,0 +1,15 @@
+class cast
+{
+public static void main(String[] args)
+{
+//type casting in java
+double d = 263; // widening (Automatically)
+System.out.println("d = "+d);
+int a =(int)7685.6544;//Narrowing
+System.out.println("a = "+a);
+int c ='c';
+System.out.println("c = "+c);
+
+}
+}
+
