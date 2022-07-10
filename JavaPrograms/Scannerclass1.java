2cc607e676cbd3d5315756cbecaf105850f70339
shrutimadduru <mvss97@gmail.com>
2022-07-10 17:17:55 -0400

    .

---
+++ JavaPrograms/Scannerclass1.java
@@ -0,0 +1,18 @@
+class Scannerclass1
+{
+public static void main(String[] args)
+{
+java.util.Scanner scn = new java.util.Scanner(System.in);
+     System.out.println("Enter the 1 st number");
+int a = scn.nextInt();
+     System.out.println("Enter the 2 nd number");
+int b = scn.nextInt();
+
+int sum = a+b;
+     System.out.println("the sum is = "+sum);
+
+
+
+}
+}
+
