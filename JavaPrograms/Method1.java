e3cf712d9a0d59990b2b3bda9bee47e8e253f0f3
shrutimadduru <mvss97@gmail.com>
2022-07-10 18:50:46 -0400

    method 1

---
+++ JavaPrograms/Method1.java
@@ -0,0 +1,24 @@
+class Method1
+{
+public static void method1(){
+
+String name = "Sai";
+System.out.println("My name is : "+name);
+
+}
+public static void method2(){
+
+System.out.println("Method 2 in a class  : ");
+
+}
+
+public static void main (String[] args)
+
+{
+
+method1();
+method2();
+
+}
+}
+
